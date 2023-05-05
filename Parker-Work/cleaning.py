8f48f7720b25fcc2c7eebb660fe9d83298e74bdb
JackmanParker <113531688+JackmanParker@users.noreply.github.com>
2023-05-05 10:30:25 -0600

    Put data in the folder with new modesl

--- Parker-Work/cleaning.py
+++ Parker-Work/cleaning.py
@@ -2,8 +2,17 @@
 #%%
 
 import pandas as pd
-
-customers = pd.read_csv('https://raw.githubusercontent.com/byui-cse/cse450-course/master/data/bank.csv')
+from sklearn.tree import DecisionTreeClassifier
+import altair as alt
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn import metrics
+from sklearn.neighbors import KNeighborsClassifier 
+customers = pd.read_csv('bankdata.csv')
 customers.head()
 
 
@@ -28,3 +37,35 @@ testClean["default"] = testClean["default"].replace(['unknown'], "no")
 testClean["housing"] = testClean["housing"].replace(['unknown'], "yes")
 testClean["loan"] = testClean["loan"].replace(['unknown'], "no")
 testClean.head()
+#%%
+#putting the data in a clean outcome thanks Jared
+testClean['poutcome_clean'] = testClean['poutcome'].apply(lambda x: 0 if x == 'nonexistent' or x == 'failure' else 1)
+testClean['pdays_clean'] = testClean['pdays'].apply(lambda x: 0 if x == 999 else x)
+filtered_data = testClean[~testClean['pdays_clean'].isna()]
+
+
+columns_list = list(testClean.columns)
+print(columns_list)
+
+# %%
+
+X = testClean.filter(items= ['job', 'education', 'poutcome_clean', 'pdays_clean', 'housing' ])
+
+
+y = testClean.filter(items= ["y"])
+
+X = pd.get_dummies(X)
+y = pd.get_dummies(y)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size= .2)
+
+
+TraingModel = KNeighborsClassifier(n_neighbors= 5)
+TraingModel.fit(X_train, y_train)
+Y_Predictions = TraingModel.predict(X_test)
+
+# conmat = metrics.confusion_matrix(y_test, Y_Predictions)
+#%%
+# metrics.ConfusionMatrixDisplay(conmat).plot()
+print("Accuracy:", metrics.accuracy_score(y_test, Y_Predictions))
+# %%
