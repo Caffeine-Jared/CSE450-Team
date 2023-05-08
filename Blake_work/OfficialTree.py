ae568658f41095c945a28a34244749cb8780ed7a
blakedennet2 <den18019@byui.edu>
2023-05-08 16:53:37 -0600

    made the csv file

--- Blake_work/OfficialTree.py
+++ Blake_work/OfficialTree.py
@@ -3,6 +3,8 @@
 #%%
 import pandas as pd
 clean = pd.read_csv('https://raw.githubusercontent.com/byui-cse/cse450-course/master/data/bank.csv')
+url = 'https://raw.githubusercontent.com/byui-cse/cse450-course/master/data/bank_holdout_test.csv'
+holdout = pd.read_csv(url)
 clean.head()
 
 
@@ -13,6 +15,7 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.model_selection import train_test_split
 from sklearn import metrics
+import numpy as np
 
 #%%
 # Encode our features and target as needed
@@ -46,10 +49,30 @@ from sklearn.metrics import classification_report
 
 report = classification_report(y_test, y_pred)
 
-predictions = pd.DataFrame(y_pred)
-
-print(type(predictions))
+holdout = pd.get_dummies(holdout[features], drop_first=True)
 
 
-print(report)
+predictions = clf.predict(holdout)
+
+
+predictions = pd.DataFrame(predictions)
+
+
+predictions[0] = np.where(predictions[0] == 'no', 0, 1)
+
+predictions.rename(columns = {0:'predictions'}, inplace = True)
+
+print(predictions.head())
+
+
+# predictions.to_csv(path_or_buf=r'C:\Users\Blake Dennett\Downloads\Spring2023\machineLearning\team_04\CSE450-Team\Blake_work\team4-module2-predictions.csv', index=False)
+
+# print(type(predictions))
+
+# with open('predictions.csv', 'w') as pred_file:
+
+#     pred_file.writelines(predictions)p
+
+
+# print(report)
 # %%
