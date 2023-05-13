d2a7818d1450c7ade7a74d2aa65f63e2bc8ae92b
Caffeine-Jared <51540063+Caffeine-Jared@users.noreply.github.com>
2023-05-12 19:23:55 -0600

    Update tree.py

--- Jared-Work/tree.py
+++ Jared-Work/tree.py
@@ -3,15 +3,15 @@ import pandas as pd
 from sklearn.utils import resample
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split, GridSearchCV
-from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.metrics import classification_report, confusion_matrix, make_scorer,recall_score
 from imblearn.over_sampling import RandomOverSampler
 import matplotlib.pyplot as plt
 from sklearn.tree import plot_tree
 
-
+# loading in the data
 clean = pd.read_csv('https://raw.githubusercontent.com/byui-cse/cse450-course/master/data/bank.csv')
 
-# Create some new features
+# Create new features - apply lambda functions to the pdays and previous columns
 def create_new_features(data):
     data['last_contact'] = data['pdays'].apply(lambda x: 1 if x == 999 else 0)
     data['recent_contact'] = data['pdays'].apply(lambda x: 1 if x < 30 else 0)
@@ -19,19 +19,22 @@ def create_new_features(data):
 
 create_new_features(clean)
 
-# Encode our features and target as needed
+# encodde features
 features = ['nr.employed', 'age', 'euribor3m', "campaign", 'cons.conf.idx', 'poutcome', 'last_contact', 'recent_contact', 'previous_contact']
 X = pd.get_dummies(clean[features], drop_first=True)
+#%%
+print(X.head())
+#%%
 y = clean['y']
-# Split our data into training and test data
+
+# Train + test split
 X_train_val, X_test, y_train_val, y_test = train_test_split(X, y, test_size=0.1, random_state=42)
 X_train, X_val, y_train, y_val = train_test_split(X_train_val, y_train_val, test_size=0.2, random_state=42)
 
-# Use RandomOverSampler for oversampling
 ros = RandomOverSampler(random_state=42)
 X_train_balanced, y_train_balanced = ros.fit_resample(X_train, y_train)
 
-# Define the parameter grid to search over
+# parameter search
 param_grid = {
     'n_estimators': [50, 100, 200],
     'max_depth': [10, 20, 30],
@@ -41,7 +44,9 @@ param_grid = {
 
 clf = RandomForestClassifier(random_state=25, n_jobs=-1)
 
-grid_search = GridSearchCV(clf, param_grid, cv=5, n_jobs=-1)
+scoring = {'recall': make_scorer(recall_score, pos_label='yes')}
+
+grid_search = GridSearchCV(clf, param_grid, cv=5, n_jobs=-1, scoring=scoring, refit='recall')
 
 grid_search.fit(X_train_balanced, y_train_balanced)
 
