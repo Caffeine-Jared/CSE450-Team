f24b0a4bafc076c7ab1297420f9efda15ca3cd72
Caffeine-Jared <51540063+Caffeine-Jared@users.noreply.github.com>
2023-05-17 13:40:26 -0600

    Update work.py

--- Project_2/Jared/work.py
+++ Project_2/Jared/work.py
@@ -3,15 +3,18 @@ from xgboost import XGBRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_absolute_error
 from sklearn.preprocessing import OneHotEncoder
+from sklearn.metrics import mean_squared_error
+from math import sqrt
+from sklearn.metrics import r2_score
 
 housing = pd.read_csv('https://raw.githubusercontent.com/byui-cse/cse450-course/master/data/housing.csv')
 
 #%%
 
-# Drop SalePrice from the dataset - ensure that the dataset is not changed, but a new dataset is created
+# Drop 'price' from the dataset - ensure that the dataset is not changed, but a new dataset is created
 X = housing.drop(['price'], axis=1)
 
-# Create the target variable, SalePrice - This is the variable we want to predict
+# Create the target variable, 'price' - This is the variable we want to predict
 y = housing['price']
 
 #%%
@@ -34,4 +37,7 @@ predictions = model.predict(X_test)
 predictions
 #%%
 print("Mean Absolute Error: " + str(mean_absolute_error(predictions, y_test)))
-
+rmse = sqrt(mean_squared_error(y_test, predictions))
+print("Root Mean Squared Error: " + str(rmse))
+r2 = r2_score(y_test, predictions)
+print("R-squared: " + str(r2))
