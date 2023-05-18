6796dee2d9ed90bb21326c7aeff6af19a64237bf
Jared Linares <51540063+Caffeine-Jared@users.noreply.github.com>
2023-05-18 15:01:18 -0600

    Update test2.py

--- Project_2/Jared/test2.py
+++ Project_2/Jared/test2.py
@@ -45,11 +45,9 @@ X_train, X_val, y_train, y_val = train_test_split(X_set, y_set, test_size=0.2)
 model = XGBRegressor(alpha=1.0)
 
 param_grid = {
-    'n_estimators': [50, 100, 200, 300, 400, 500],
-    'max_depth': [6, 10, 15, 20],
+    'n_estimators': [50, 100, 200, 250, 300],
+    'max_depth': [6, 10, 15, 20, 25, 30],
     'learning_rate': [0.01, 0.1, 0.3],
-    'subsample': [0.5, 0.7, 1.0],
-    'colsample_bytree': [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
 }
 
 # specify number of loops
@@ -59,28 +57,58 @@ best_scores = []
 best_models = []
 best_features = []
 
-# Get list of column names you want to consider for feature selection
 column_names = X.columns.tolist()
 
 # Loop through all lengths of combinations
 for r in range(1, len(column_names) + 1):
-    # Loop through all combinations of columns of length r
     for cols in combinations(column_names, r):
         for i in range(num_loops):
-            # Use only the columns in this combination
             X_train_temp = X_train[list(cols)]
             X_val_temp = X_val[list(cols)]
 
             grid_search = GridSearchCV(estimator=model, param_grid=param_grid, cv=3, n_jobs=3, scoring='neg_mean_squared_error')
             grid_search.fit(X_train_temp, y_train)
 
-            # Get the best model from this run and store it
-            best_scores.append(-grid_search.best_score_)  # Flip the sign to make MSE positive
+            # store best model and score
+            best_scores.append(-grid_search.best_score_)
             best_models.append(grid_search.best_estimator_)
             best_features.append(cols)  # Store the used features
+            print("Loop: ", i)
+            print("Best score: ", -grid_search.best_score_)
+            print("Best model: ", grid_search.best_estimator_)
+            print("Best features: ", cols)
+            print("")
+            # Print results so far
+            #$sdfgsdfgsdfgsdfgsdfgsdrfgsdfgsdfgsdfgsdfgssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss
+                  # Print results so far
+            #$sdfgsdfgsdfgsdfgsdfgsdrfgsdfgsdfgsdfgsdfgssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss
+                  # Print results so far
+            #$sdfgsdfgsdfgsdfgsdfgsdrfgsdfgsdfgsdfgsdfgssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss
+                  # Print results so far
+            #$sdfgsdfgsdfgsdfgsdfgsdrfgsdfgsdfgsdfgsdfgssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss
+                  # Print results so far
+            #$sdfgsdfgsdfgsdfgsdfgsdrfgsdfgsdfgsdfgsdfgssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss
+                  # Print results so far
+            #$sdfgsdfgsdfgsdfgsdfgsdrfgsdfgsdfgsdfgsdfgssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss
+                  # Print results so far
+            #$sdfgsdfgsdfgsdfgsdfgsdrfgsdfgsdfgsdfgsdfgssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss
+            print("Results so far: ")
+            print("Best scores: ", best_scores)
+            print("Best models: ", best_models)
+            print("Best features: ", best_features)
+            with open('output.txt', 'a') as f:
+                print("Loop: ", i, file=f)
+                print("Best score: ", -grid_search.best_score_, file=f)
+                print("Best model: ", grid_search.best_estimator_, file=f)
+                print("Best features: ", cols, file=f)
+                print("", file=f)
+                print("Results so far: ", file=f)
+                print("Best scores: ", best_scores, file=f)
+                print("Best models: ", best_models, file=f)
+                print("Best features: ", best_features, file=f)
 
 # Get index of the model with the best score
-best_index = np.argmin(best_scores)  # We use argmin because we want to minimize MSE
+best_index = np.argmin(best_scores)
 
 # Select the best model and best features
 best_model_overall = best_models[best_index]
@@ -90,7 +118,7 @@ best_features_overall = best_features[best_index]
 print(best_model_overall.get_params())
 print("Best features: ", best_features_overall)
 
-X_test_temp = X_test[list(best_features_overall)]  # Test the model on the same features as those it was trained on
+X_test_temp = X_test[list(best_features_overall)]
 y_pred = best_model_overall.predict(X_test_temp)
 print(y_pred)
 print("Mean Absolute Error: " + str(mean_absolute_error(y_pred, y_test)))
