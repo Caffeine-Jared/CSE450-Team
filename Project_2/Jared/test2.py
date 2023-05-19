a92f13b369bc30fca9c7e5e85e753f2a136c9e7e
Jared Linares <51540063+Caffeine-Jared@users.noreply.github.com>
2023-05-19 15:03:50 -0600

    update

--- Project_2/Jared/test2.py
+++ Project_2/Jared/test2.py
@@ -9,11 +9,14 @@ from sklearn.metrics import r2_score
 import numpy as np
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.preprocessing import MinMaxScaler
+from tune_sklearn import TuneGridSearchCV
 
 housing = pd.read_csv('https://raw.githubusercontent.com/byui-cse/cse450-course/master/data/housing.csv')
 
+# add built_after_1976 column to dataframe - hot encode it
 housing['built_after_1976'] = (housing['yr_built'] > 1976).astype(int)
 
+# calculate distance from each location
 def calculate_distance(lat1, lon1, lat2, lon2):
     return geodesic((lat1, lon1), (lat2, lon2)).miles
 
@@ -24,50 +27,62 @@ locations = {
     "Boeing_Plant": (47.543969, -122.316443)
 }
 
+# add distance columns to dataframe
 for name, coords in locations.items():
     housing[name + '_distance'] = housing.apply(lambda row: calculate_distance(row['lat'], row['long'], coords[0], coords[1]), axis=1)
 
+# add sqft_product, year, month, and day columns to dataframe
 housing['sqft_product'] = housing['sqft_living'] * housing['sqft_lot']
 housing['year'] = pd.to_datetime(housing['date']).dt.year
 housing['month'] = pd.to_datetime(housing['date']).dt.month
 housing['day'] = pd.to_datetime(housing['date']).dt.day
 
+# drop date column
 housing = housing.drop('date', axis=1)
 
 # Bin sqft_product into different categories
 housing['sqft_product_bins'] = pd.qcut(housing['sqft_product'], q=3, labels=['small_properties', 'medium_properties', 'large_properties'])
 
+# hot encode sqft_product_bins
 housing = pd.get_dummies(housing, columns=['sqft_product_bins'])
 
 X = housing.drop('price', axis=1)
 y = housing['price']
 
+# scale all columns - not sure if this is necessary
 scaler = MinMaxScaler()
 
+# add scaled columns to dataframe
 for col in X.columns:
     X[col + '_scaled'] = scaler.fit_transform(X[[col]])
 
 X_set, X_test, y_set, y_test = train_test_split(X, y, test_size=0.1)
 X_train, X_val, y_train, y_val = train_test_split(X_set, y_set, test_size=0.2)
 
-model = XGBRegressor(alpha=1.0)
+# define model - alpha=1.0 is default
+model = XGBRegressor()
 
+# define parameters to search
 param_grid = {
     'n_estimators': [50, 100, 200, 250, 300],
     'max_depth': [6, 10, 15, 20, 25, 30],
     'learning_rate': [0.01, 0.1, 0.3],
 }
 
+# define target values - these are the values we want to reach (potentially)
 target_mae = 10000
 target_rmse = 50000
 target_r2 = 0.95
 
+# define initial values - these are the values we start with
 mae = float('inf')
 rmse = float('inf')
 r2 = 0
 
+# loop until we reach target values
 while mae > target_mae or rmse > target_rmse or r2 < target_r2:
-    grid_search = GridSearchCV(estimator=model, param_grid=param_grid, cv=3, n_jobs=-1, scoring='neg_mean_squared_error')
+    # define grid search - scoring is negative mean squared error, cv=3 is 3-fold cross validation, n_jobs=-1 is to use all processors
+    grid_search = TuneGridSearchCV(estimator=model, param_grid=param_grid, cv=3, n_jobs=-1, scoring='neg_mean_squared_error')
     grid_search.fit(X_train, y_train)
 
     feature_importance = grid_search.best_estimator_.feature_importances_
@@ -78,6 +93,7 @@ while mae > target_mae or rmse > target_rmse or r2 < target_r2:
     })
 
     # FIRST SUB-100k features: Top features:  ['grade', 'waterfront', 'sqft_living', 'Amazon_HQ_distance', 'lat', 'view', 'Starbucks_distance', 'Boeing_Plant_distance', 'sqft_living15', 'Microsoft_distance', 'sqft_product', 'sqft_above', 'year', 'yr_built', 'zipcode', 'bathrooms', 'condition', 'long', 'sqft_lot15', 'yr_renovated', 'floors', 'sqft_lot']
+    # importance_df - 22 features -  go
     importance_df = importance_df.sort_values('Importance', ascending=False)
     n = 22
     top_features = importance_df['Feature'].head(n).tolist()
@@ -88,6 +104,7 @@ while mae > target_mae or rmse > target_rmse or r2 < target_r2:
 
     grid_search.fit(X_train_selected, y_train)
 
+#     printing results
     y_pred = grid_search.predict(X_test_selected)
     mae = mean_absolute_error(y_pred, y_test)
     print("Mean Absolute Error: " + str(mae))
@@ -96,13 +113,22 @@ while mae > target_mae or rmse > target_rmse or r2 < target_r2:
     r2 = r2_score(y_test, y_pred)
     print("R-squared: " + str(r2))
     print("\n")
+    print("n_estimators: " + str(grid_search.best_estimator_.n_estimators))
+    print("max_depth: " + str(grid_search.best_estimator_.max_depth))
+    print("learning_rate: " + str(grid_search.best_estimator_.learning_rate))
+    print("\n")
     
+    # write results to file - append to file
     with open('output.txt', 'a') as f:
         print("Number of features: ", n, file=f)
         print("Top features: ", top_features, file=f)
         print("Mean Absolute Error: " + str(mae), file=f)
         print("Root Mean Squared Error: " + str(rmse), file=f)
         print("R-squared: " + str(r2), file=f)
+        print("\n", file=f)
+        print("n_estimators: " + str(grid_search.best_estimator_.n_estimators), file=f)
+        print("max_depth: " + str(grid_search.best_estimator_.max_depth), file=f)
+        print("learning_rate: " + str(grid_search.best_estimator_.learning_rate), file=f)
         print("\n", file=f)
     if mae <= target_mae and rmse <= target_rmse and r2 >= target_r2:
         break
