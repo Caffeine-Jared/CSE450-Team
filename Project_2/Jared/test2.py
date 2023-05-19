ca909d6b871fae2d6b1f463c6a8a7e41c538d412
Caffeine-Jared <51540063+Caffeine-Jared@users.noreply.github.com>
2023-05-18 19:48:21 -0600

    update

--- Project_2/Jared/test2.py
+++ Project_2/Jared/test2.py
@@ -7,7 +7,6 @@ from sklearn.metrics import mean_squared_error
 from math import sqrt
 from sklearn.metrics import r2_score
 import numpy as np
-from itertools import combinations
 
 housing = pd.read_csv('https://raw.githubusercontent.com/byui-cse/cse450-course/master/data/housing.csv')
 
@@ -25,9 +24,6 @@ locations = {
 
 for name, coords in locations.items():
     housing[name + '_distance'] = housing.apply(lambda row: calculate_distance(row['lat'], row['long'], coords[0], coords[1]), axis=1)
-
-column_names = housing.columns.tolist()
-
 
 housing['sqft_product'] = housing['sqft_living'] * housing['sqft_lot']
 housing['year'] = pd.to_datetime(housing['date']).dt.year
@@ -50,76 +46,37 @@ param_grid = {
     'learning_rate': [0.01, 0.1, 0.3],
 }
 
-# specify number of loops
-num_loops = 1
+grid_search = GridSearchCV(estimator=model, param_grid=param_grid, cv=3, n_jobs=-1, scoring='neg_mean_squared_error')
+grid_search.fit(X_train, y_train)
 
-best_scores = []
-best_models = []
-best_features = []
+# Get feature importance
+feature_importance = grid_search.best_estimator_.feature_importances_
 
-column_names = X.columns.tolist()
+# Create a pandas DataFrame with the feature importance
+importance_df = pd.DataFrame({
+    'Feature': X.columns,
+    'Importance': feature_importance
+})
 
-# Loop through all lengths of combinations
-for r in range(1, len(column_names) + 1):
-    for cols in combinations(column_names, r):
-        for i in range(num_loops):
-            X_train_temp = X_train[list(cols)]
-            X_val_temp = X_val[list(cols)]
+# Sort the DataFrame by the importance in descending order
+importance_df = importance_df.sort_values('Importance', ascending=False)
 
-            grid_search = GridSearchCV(estimator=model, param_grid=param_grid, cv=3, n_jobs=3, scoring='neg_mean_squared_error')
-            grid_search.fit(X_train_temp, y_train)
+# Select top-n features
+n = 10
+top_features = importance_df['Feature'].head(n).tolist()
 
-            # store best model and score
-            best_scores.append(-grid_search.best_score_)
-            best_models.append(grid_search.best_estimator_)
-            best_features.append(cols)  # Store the used features
-            print("Loop: ", i)
-            print("Best score: ", -grid_search.best_score_)
-            print("Best model: ", grid_search.best_estimator_)
-            print("Best features: ", cols)
-            print("")
-            # Print results so far
-            #$sdfgsdfgsdfgsdfgsdfgsdrfgsdfgsdfgsdfgsdfgssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss
-                  # Print results so far
-            #$sdfgsdfgsdfgsdfgsdfgsdrfgsdfgsdfgsdfgsdfgssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss
-                  # Print results so far
-            #$sdfgsdfgsdfgsdfgsdfgsdrfgsdfgsdfgsdfgsdfgssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss
-                  # Print results so far
-            #$sdfgsdfgsdfgsdfgsdfgsdrfgsdfgsdfgsdfgsdfgssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss
-                  # Print results so far
-            #$sdfgsdfgsdfgsdfgsdfgsdrfgsdfgsdfgsdfgsdfgssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss
-                  # Print results so far
-            #$sdfgsdfgsdfgsdfgsdfgsdrfgsdfgsdfgsdfgsdfgssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss
-                  # Print results so far
-            #$sdfgsdfgsdfgsdfgsdfgsdrfgsdfgsdfgsdfgsdfgssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss
-            print("Results so far: ")
-            print("Best scores: ", best_scores)
-            print("Best models: ", best_models)
-            print("Best features: ", best_features)
-            with open('output.txt', 'a') as f:
-                print("Loop: ", i, file=f)
-                print("Best score: ", -grid_search.best_score_, file=f)
-                print("Best model: ", grid_search.best_estimator_, file=f)
-                print("Best features: ", cols, file=f)
-                print("", file=f)
-                print("Results so far: ", file=f)
-                print("Best scores: ", best_scores, file=f)
-                print("Best models: ", best_models, file=f)
-                print("Best features: ", best_features, file=f)
+# Print the top features
+print("Top features: ", top_features)
 
-# Get index of the model with the best score
-best_index = np.argmin(best_scores)
+# Select these features from your dataset
+X_train_selected = X_train[top_features]
+X_val_selected = X_val[top_features]
+X_test_selected = X_test[top_features]
 
-# Select the best model and best features
-best_model_overall = best_models[best_index]
-best_features_overall = best_features[best_index]
+# Refit the model on the selected features
+grid_search.fit(X_train_selected, y_train)
 
-# Print best model's parameters
-print(best_model_overall.get_params())
-print("Best features: ", best_features_overall)
-
-X_test_temp = X_test[list(best_features_overall)]
-y_pred = best_model_overall.predict(X_test_temp)
+y_pred = grid_search.predict(X_test_selected)
 print(y_pred)
 print("Mean Absolute Error: " + str(mean_absolute_error(y_pred, y_test)))
 rmse = sqrt(mean_squared_error(y_test, y_pred))
